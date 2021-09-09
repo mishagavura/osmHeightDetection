547216469afe1fab7d0cd38210b0baf4d2312b58
Misha Gavura <43016416+mishagavura@users.noreply.github.com>
2021-09-10 00:44:59 +0300

    Add files via upload

--- osm/main.py
+++ osm/main.py
@@ -13,6 +13,7 @@ for i in range(1,7):
 	exec(f'yestxt{i}.write("")')
 	exec(f'result{i} = overpass.query(query{i})')
 	exec(f'res_{i} = 0')
+	exec(f'written_{i} = 0')
 
 for item in result1.elements():
 	try:
@@ -26,6 +27,7 @@ for item in result1.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('yes.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_1 += 1
 		except Exception:
 			pass
 for item in result2.elements():
@@ -40,6 +42,7 @@ for item in result2.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('apartments.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_2 += 1
 		except Exception:
 			pass
 for item in result3.elements():
@@ -54,6 +57,7 @@ for item in result3.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('service.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_3 += 1
 		except Exception:
 			pass
 for item in result4.elements():
@@ -68,6 +72,7 @@ for item in result4.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('school.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_4 += 1
 		except Exception:
 			pass
 for item in result5.elements():
@@ -82,6 +87,7 @@ for item in result5.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('kindergarten.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_5 += 1
 		except Exception:
 			pass
 for item in result6.elements():
@@ -96,14 +102,15 @@ for item in result6.elements():
 			r_write = item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] + '\n'
 			yestxt = open('industrial.txt', 'a', encoding='utf-8')
 			yestxt.write(r_write)
+			written_6 += 1
 		except Exception:
 			pass
 
-print(f'Building = yes          =  {result1.countElements()}, without height = {res_1}')
-print(f'Building = apartments   =  {result2.countElements()}, without height = {res_2}')
-print(f'Building = service      =  {result3.countElements()}, without height = {res_3}')
-print(f'Building = school       =  {result4.countElements()}, without height = {res_4}')
-print(f'Building = kindergarten =  {result5.countElements()}, without height = {res_5}')
-print(f'Building = industrial   =  {result6.countElements()}, without height = {res_6}')
+print(f'Building = yes          =  {result1.countElements()}, without height = {res_1}, written in txt = {written_1}')
+print(f'Building = apartments   =  {result2.countElements()}, without height = {res_2}, written in txt = {written_2}')
+print(f'Building = service      =  {result3.countElements()}, without height = {res_3}, written in txt = {written_3}')
+print(f'Building = school       =  {result4.countElements()}, without height = {res_4}, written in txt = {written_4}')
+print(f'Building = kindergarten =  {result5.countElements()}, without height = {res_5}, written in txt = {written_5}')
+print(f'Building = industrial   =  {result6.countElements()}, without height = {res_6}, written in txt = {written_6}')
 print('Sum of all  =  ' + str(int(result1.countElements())+int(result2.countElements())+int(result3.countElements())+int(result4.countElements())+int(result5.countElements())+int(result6.countElements())))
 print('Sum of only without height  =  ' + str(int(res_1+res_2+res_3+res_4+res_5+res_6)))
