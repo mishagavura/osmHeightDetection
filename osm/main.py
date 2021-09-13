8ea550796d028ce3df029829334fe5a13fdc3f9a
Misha Gavura <43016416+mishagavura@users.noreply.github.com>
2021-09-13 17:31:41 +0300

    type y/n

--- osm/main.py
+++ osm/main.py
@@ -15,103 +15,205 @@ for i in range(1,7):
 	exec(f'res_{i} = 0')
 	exec(f'written_{i} = 0')
 
-for item in result1.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_1 += 1
-		r_write = ''
-		try:
-			print(item.id())
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] +  '\n'
-			yestxt = open('yes.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_1 += 1
-		except Exception:
-			pass
-for item in result2.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_2 += 1
-		r_write = ''
-		try:
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']   + '\n'
-			yestxt = open('apartments.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_2 += 1
-		except Exception:
-			pass
-for item in result3.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_3 += 1
-		r_write = ''
-		try:
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']   + '\n'
-			yestxt = open('service.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_3 += 1
-		except Exception:
-			pass
-for item in result4.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_4 += 1
-		r_write = ''
-		try:
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  +  '\n' 
-			yestxt = open('school.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_4 += 1
-		except Exception:
-			pass
-for item in result5.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_5 += 1
-		r_write = ''
-		try:
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  +  '\n'
-			yestxt = open('kindergarten.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_5 += 1
-		except Exception:
-			pass
-for item in result6.elements():
-	try:
-		if item.tags()['building:levels'] != '0':
-			pass
-	except Exception:
-		#print(item.tags())
-		res_6 += 1
-		r_write = ''
-		try:
-			r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  + '\n'
-			yestxt = open('industrial.txt', 'a', encoding='utf-8')
-			yestxt.write(r_write)
-			written_6 += 1
-		except Exception:
-			pass
+adres = input('Would you like to see adresses? type y/n')
+if adres == 'y':
 
-print(f'Building = yes          =  {result1.countElements()}, without height = {res_1}, written in txt = {written_1}')
-print(f'Building = apartments   =  {result2.countElements()}, without height = {res_2}, written in txt = {written_2}')
-print(f'Building = service      =  {result3.countElements()}, without height = {res_3}, written in txt = {written_3}')
-print(f'Building = school       =  {result4.countElements()}, without height = {res_4}, written in txt = {written_4}')
-print(f'Building = kindergarten =  {result5.countElements()}, without height = {res_5}, written in txt = {written_5}')
-print(f'Building = industrial   =  {result6.countElements()}, without height = {res_6}, written in txt = {written_6}')
-print('Sum of all  =  ' + str(int(result1.countElements())+int(result2.countElements())+int(result3.countElements())+int(result4.countElements())+int(result5.countElements())+int(result6.countElements())))
-print('Sum of only without height  =  ' + str(int(res_1+res_2+res_3+res_4+res_5+res_6)))
+	for item in result1.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_1 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber'] +  '\n'
+				yestxt = open('yes.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_1 += 1
+			except Exception:
+				pass
+	for item in result2.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_2 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']   + '\n'
+				yestxt = open('apartments.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_2 += 1
+			except Exception:
+				pass
+	for item in result3.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_3 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']   + '\n'
+				yestxt = open('service.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_3 += 1
+			except Exception:
+				pass
+	for item in result4.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_4 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  +  '\n' 
+				yestxt = open('school.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_4 += 1
+			except Exception:
+				pass
+	for item in result5.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_5 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  +  '\n'
+				yestxt = open('kindergarten.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_5 += 1
+			except Exception:
+				pass
+	for item in result6.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_6 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + "   :   "+ item.tags()['addr:street'] + ' ' + item.tags()['addr:housenumber']  + '\n'
+				yestxt = open('industrial.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_6 += 1
+			except Exception:
+				pass
+
+	print(f'Building = yes          =  {result1.countElements()}, without height = {res_1}, written in txt = {written_1}')
+	print(f'Building = apartments   =  {result2.countElements()}, without height = {res_2}, written in txt = {written_2}')
+	print(f'Building = service      =  {result3.countElements()}, without height = {res_3}, written in txt = {written_3}')
+	print(f'Building = school       =  {result4.countElements()}, without height = {res_4}, written in txt = {written_4}')
+	print(f'Building = kindergarten =  {result5.countElements()}, without height = {res_5}, written in txt = {written_5}')
+	print(f'Building = industrial   =  {result6.countElements()}, without height = {res_6}, written in txt = {written_6}')
+	print('Sum of all  =  ' + str(int(result1.countElements())+int(result2.countElements())+int(result3.countElements())+int(result4.countElements())+int(result5.countElements())+int(result6.countElements())))
+	print('Sum of only without height  =  ' + str(int(res_1+res_2+res_3+res_4+res_5+res_6)))
+else:
+	for item in result1.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_1 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) + '\n'
+				yestxt = open('yes.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_1 += 1
+			except Exception:
+				pass
+	for item in result2.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_2 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id())   + '\n'
+				yestxt = open('apartments.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_2 += 1
+			except Exception:
+				pass
+	for item in result3.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_3 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id())   + '\n'
+				yestxt = open('service.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_3 += 1
+			except Exception:
+				pass
+	for item in result4.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_4 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id())  +  '\n' 
+				yestxt = open('school.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_4 += 1
+			except Exception:
+				pass
+	for item in result5.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_5 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id()) +  '\n'
+				yestxt = open('kindergarten.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_5 += 1
+			except Exception:
+				pass
+	for item in result6.elements():
+		try:
+			if item.tags()['building:levels'] != '0':
+				pass
+		except Exception:
+			#print(item.tags())
+			res_6 += 1
+			r_write = ''
+			try:
+				r_write = str(item.id())  + '\n'
+				yestxt = open('industrial.txt', 'a', encoding='utf-8')
+				yestxt.write(r_write)
+				written_6 += 1
+			except Exception:
+				pass
+
+	print(f'Building = yes          =  {result1.countElements()}, without height = {res_1}, written in txt = {written_1}')
+	print(f'Building = apartments   =  {result2.countElements()}, without height = {res_2}, written in txt = {written_2}')
+	print(f'Building = service      =  {result3.countElements()}, without height = {res_3}, written in txt = {written_3}')
+	print(f'Building = school       =  {result4.countElements()}, without height = {res_4}, written in txt = {written_4}')
+	print(f'Building = kindergarten =  {result5.countElements()}, without height = {res_5}, written in txt = {written_5}')
+	print(f'Building = industrial   =  {result6.countElements()}, without height = {res_6}, written in txt = {written_6}')
+	print('Sum of all  =  ' + str(int(result1.countElements())+int(result2.countElements())+int(result3.countElements())+int(result4.countElements())+int(result5.countElements())+int(result6.countElements())))
+	print('Sum of only without height  =  ' + str(int(res_1+res_2+res_3+res_4+res_5+res_6)))
